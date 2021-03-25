bd6acfae035f77aa377264fd2e5e6be74a7f5b51
Michael Kerry <mkerry@fas.harvard.edu>
2021-03-25 11:07:57 -0400

    revise filter handling

--- pylog/pylog.py
+++ pylog/pylog.py
@@ -41,24 +41,24 @@ def get_common_logger_for_module(module_name: str, level: int = 0, log_format: l
     module_logger.setLevel(level=level)
 
     info_stream_handler = logging.StreamHandler(stream=sys.stdout)
+    info_stream_handler.setFormatter(fmt=log_format)
 
     if level > logging.INFO:
         info_stream_handler.setLevel(level=logging.INFO)
     else:
         info_stream_handler.setLevel(level=level)
 
-    info_stream_handler.addFilter(filter=InfoFilter)
-    info_stream_handler.setFormatter(fmt=log_format)
+    info_stream_handler.addFilter(InfoFilter())
     module_logger.addHandler(hdlr=info_stream_handler)
 
     stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(fmt=log_format)
 
     if level > logging.WARNING:
         stream_handler.setLevel(level=level)
     else:
         stream_handler.setLevel(level=logging.WARNING)
 
-    stream_handler.setFormatter(fmt=log_format)
     module_logger.addHandler(hdlr=stream_handler)
 
     return module_logger
