555ca83c37cd5b88b298015abfecf63b013df9ef
Michael Kerry <mkerry@fas.harvard.edu>
2021-04-13 11:52:05 -0400

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pylog",
-    version="0.0.2",
+    version="0.0.3",
     author="Michael Kerry",
     author_email="michael_kerry@harvard.edu",
     description="A utility for python logging",
